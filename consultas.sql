969bebff54b6ddfadc285eeb06eb58cc354ae14f
Emanuel Cruz <emanuel@cyberargsistemas.com.ar>
2022-10-26 23:32:37 -0300

    insert tipo_variante, tipo_descuento_venta, medio_envio, medio_pago_venta, tipo_cupon

---
+++ consultas.sql
@@ -0,0 +1,20 @@
+select *
+from sqlnt.CATEGORIA;
+
+select *
+from sqlnt.PRODUCTO;
+
+select *
+from sqlnt.TIPO_VARIANTE;
+
+select *
+from sqlnt.TIPO_DESCUENTO_VENTA;
+
+select *
+from sqlnt.MEDIO_ENVIO;
+
+select *
+from sqlnt.MEDIO_PAGO_VENTA;
+
+select *
+from sqlnt.TIPO_CUPON;
